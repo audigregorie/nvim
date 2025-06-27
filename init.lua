300b4465613154e6050f06b5ac5b5508070d9ea1
Audi Young <122812915+audigregorie@users.noreply.github.com>
2025-06-27 02:04:37 +0100

    feat: nvim-cmp priority

--- init.lua
+++ init.lua
@@ -468,11 +468,10 @@ require("lazy").setup({
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        }, {
-          { name = "buffer" },
-          { name = "path" },
+          { name = "nvim_lsp", priority = 1000 },
+          { name = "luasnip",  priority = 750 },
+          { name = "buffer",   priority = 500 },
+          { name = "path",     priority = 250 },
         }),
         formatting = {
           format = lspkind.cmp_format({
